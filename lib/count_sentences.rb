78dab3d8d39bdc335d870e65f7fb6dc1ad0e0016
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-04 20:23:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,7 +20,7 @@ class String
 
   def count_sentences
     #goal: return # of sentences 
-    binding.pry
+    # binding.pry
     # self.split(/[!.?]/).count 
     delimiters = [".", "?", "!"]
     self.split(Regexp.union(delimiters))
