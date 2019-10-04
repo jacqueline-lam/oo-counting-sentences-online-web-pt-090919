b3778fdb0e4de46ad4862ce3a153fac64c737e3c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-04 20:25:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,25 +20,8 @@ class String
 
   def count_sentences
     #goal: return # of sentences 
-    # binding.pry
     # self.split(/[!.?]/).count 
     delimiters = [". ", "? ", "! "]
     self.split(Regexp.union(delimiters)).count
   end
 end
-
-# describe "#count_sentences" do
-
-#     it  "returns the number of sentences in a string" do
-#       expect("one. two. three?".count_sentences).to eq(3)
-#     end
-
-#     it "returns zero if there are no sentences in a string" do
-#       expect("".count_sentences).to eq(0)
-#     end
-
-#     it "returns the number of sentences in a complex string" do
-#       complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-#       expect(complex_string.count_sentences).to eq(4)
-#     end
-#   end
